f1f9f6766c00cf5ac9d01d4cd98bb295d5479d3c
Pavel Gamov <pagamov@gmail.com>
2021-03-06 23:38:15 +0300

    40

--- index.py
+++ index.py
@@ -1,29 +1,33 @@
 from lib import eratosthenes,prime
 from time import time
 
-def ans_trig(p):
-    ans = []
-    for a in range(1,p):
-        for b in range(1,p):
-            c = p - a - b
-            print "\r",p,a,b,c,
-            if c > 0:
-                if a*a+b*b==c*c:
-                    if sorted([a,b,c]) not in ans:
-                        ans.append(sorted([a,b,c]))
-    return ans
+def champernowne(n):
+    i = 1
+    s = 1
+    s_ = str(s)
+    while True:
+        ip = 0
+        #print("work ",s_)
+        for c in s_:
+            #print(c)
+            if i == n:
+                return int(c)
+            i+=1
+        s+=1
+        s_=str(s)
 
-# print ans_trig(0)
-# exit()
-m = -1
-p_m = -1
-t = time()
-for p in range(1000+1):
-    # print "\r",p,
-    a = ans_trig(p)
-    if len(a) > m:
-        print "\n",p, a
-        m = len(a)
-        p_m = p
-print time() - t, " sec"
-print "\nans: ", p_m
+res = []
+for i in range(7):
+    t = time()
+    print "work: ", 10**i
+    r = champernowne(10**i)
+    print r, time()-t
+    res.append(r)
+
+ans = 1
+for n in res:
+    ans *= n
+print("ans: ",ans)
+print(sum(res))
+exit()
+

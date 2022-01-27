7cd9dd475a7c101857be6bc9936318716df0548f
Pavel <52429594+pagamov@users.noreply.github.com>
2022-01-27 22:40:28 +0300

    55

--- index.py
+++ index.py
@@ -2,4 +2,28 @@ from lib import eratosthenes, prime, Pandig, pandig
 from time import time
 
 
-print(str(28433*2**7830457+1)[::-1][:12])
+
+
+arr = []
+n = 0
+while True:
+    piv = n
+    it = 0
+    while True:
+        if it > 50:
+            arr.append(n)
+            break
+        if piv == int(str(piv)[::-1]) and it == 0:
+            arr.append(n)
+            break
+        elif piv == int(str(piv)[::-1]) and it != 0:
+            break
+        else:
+            piv += int(str(piv)[::-1])
+            it += 1
+
+    if n >= 10000:
+        break
+    n += 1
+print(len(arr))
+# print(arr)

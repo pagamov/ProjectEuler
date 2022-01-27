8f37ecbb44ed462ae1578c09d84f7c1aaf699b99
Pavel <52429594+pagamov@users.noreply.github.com>
2022-01-27 19:54:51 +0300

    97

--- index.py
+++ index.py
@@ -2,14 +2,4 @@ from lib import eratosthenes, prime, Pandig, pandig
 from time import time
 
 
-def spl(x):
-    return set(sorted(list(map(int,[x for x in str(x)]))))
-
-piv = 1
-while True:
-    s = spl(piv)
-    if s == spl(2*piv) and s == spl(3*piv) and s == spl(4*piv) and s == spl(5*piv) and s == spl(6*piv):
-        print(piv)
-        exit()
-
-    piv += 1
+print(str(28433*2**7830457+1)[::-1][:12])

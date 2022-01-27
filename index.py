5aa6b5b57bf5f344e22e2f6cef9f134ebbe42c70
Pavel <52429594+pagamov@users.noreply.github.com>
2022-01-27 17:34:45 +0300

    63

--- index.py
+++ index.py
@@ -2,30 +2,16 @@ from lib import eratosthenes, prime, Pandig
 from time import time
 
 
-def contains(n):
-    p = eratosthenes(n)
-    res = []
-    while n != 1:
-        for i in range(len(p)):
-            if n % p[i] == 0:
-                while n % p[i] == 0:
-                    n = n / p[i]
-                if p[i] not in res:
-                    res.append(p[i])
-    return res
+def t(n, p):
+    piv = n**p
+    if len(str(piv)) == p:
+        return True
+    return False
 
+g = 0
+for n in range(1,1000):
+    for p in range(1,1000):
+        if t(n,p):
+            g += 1
 
-curr = 13
-
-while True:
-    print('\r',curr,end='')
-    f = True
-    for i in range(curr,curr + 4):
-        if len(contains(i)) != 4:
-            f = False
-            break
-    if f:
-        print('\n',curr)
-        exit()
-
-    curr += 1
+print(g)

b0dcc8d01d0551162dda8bfb23bd9d5c15eee998
Pavel <52429594+pagamov@users.noreply.github.com>
2022-01-27 22:49:19 +0300

    53

--- index.py
+++ index.py
@@ -2,28 +2,16 @@ from lib import eratosthenes, prime, Pandig, pandig
 from time import time
 
 
+def fac(x):
+    if x == 0:
+        return 1
+    if x == 1:
+        return 1
+    return fac(x-1) * x
 
-
-arr = []
-n = 0
-while True:
-    piv = n
-    it = 0
-    while True:
-        if it > 50:
-            arr.append(n)
-            break
-        if piv == int(str(piv)[::-1]) and it == 0:
-            arr.append(n)
-            break
-        elif piv == int(str(piv)[::-1]) and it != 0:
-            break
-        else:
-            piv += int(str(piv)[::-1])
-            it += 1
-
-    if n >= 10000:
-        break
-    n += 1
-print(len(arr))
-# print(arr)
+i = 0
+for n in range(1, 101):
+    for r in range(1, n+1):
+        if fac(n) / (fac(r)*fac(n-r)) > 1000000:
+            i += 1
+print(i)
